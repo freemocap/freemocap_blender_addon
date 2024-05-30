d77575f62204b5e3c64a6d142d2fb761e738990c
jonmatthis <jonmatthis@gmail.com>
2024-05-30 11:26:38 -0400

    commit partially broken stats stuff

--- ajc27_freemocap_blender_addon/data_models/sample_statistics.py
+++ ajc27_freemocap_blender_addon/data_models/sample_statistics.py
@@ -6,30 +6,64 @@ import numpy as np
 Z_SCORE_95_CI = 1.96  # Z-score for 95% confidence interval
 Z_SCORE_99_CI = 2.58  # Z-score for 99% confidence interval
 
-Samples = List[float]  # custom type alias for a list of float samples
+SampleList = List[float]  # custom type alias for a list of float samples
+
+
+@dataclass
+class SampleData:
+    name: str
+    data: np.ndarray
+
+    def __post_init__(self):
+        if self.data:
+            raise ValueError("Sample list cannot be empty")
+        if len(self.data.shape) != 1:
+            raise ValueError("Sample list must be one-dimensional (for now)")
+        if not np.any(self.data):
+            raise ValueError("Sample list cannot be empty")
+        if np.all(np.isnan(self.data)):
+            raise ValueError("Sample list cannot contain only NaN values")
+        if np.all(np.isinf(self.data)):
+            raise ValueError("Sample list cannot contain only infinite values")
+        if not np.issubdtype(self.data.dtype, np.number):
+            raise ValueError("Sample list must contain only numerical values")
+
+    @classmethod
+    def from_samples(cls,
+                     name: str,
+                     samples: SampleList) -> 'SampleData':
+        return cls(
+            name=name,
+            data=np.array(samples)
+        )
+    @property
+    def samples(self) -> SampleList:
+        return self.data.tolist()
+
+    @property
+    def number_of_samples(self) -> int:
+        return len(self.samples)
+
+    def __repr__(self) -> str:
+        return f"SampleData(data={self.data.shape})"
 
 
 @dataclass
 class CentralTendencyMeasures:
-    data: np.ndarray
     mean: float  # Arithmetic mean, simplest measure of central tendency
     median: float  # Middle value of a dataset, robust to outliers
 
     @classmethod
-    def from_data(cls, data: np.ndarray) -> 'CentralTendencyMeasures':
-        if not data:
-            raise ValueError("Sample list cannot be empty")
-        if len(data.shape) != 1:
-            raise ValueError("Sample list must be one-dimensional")
-
+    def from_data(cls,
+                  data: np.ndarray) -> 'CentralTendencyMeasures':
         return cls(
-            data=data,
             mean=np.nanmean(data),
             median=np.nanmedian(data),
         )
 
     @classmethod
-    def from_samples(cls, samples: Samples) -> 'CentralTendencyMeasures':
+    def from_samples(cls,
+                     samples: SampleList) -> 'CentralTendencyMeasures':
         if not samples:
             raise ValueError("Sample list cannot be empty")
         data = np.array(samples)
@@ -44,20 +78,20 @@ class VariabilityMeasures:
     """
     Class for measuring variability in a dataset.
     """
-    data: np.ndarray
     stddev: float  # Standard deviation, measures the dispersion of a dataset
     mad: float  # Median absolute deviation, robust to outliers
     iqr: float  # Interquartile range, robust measure of dispersion
     ci95: float  # 95% confidence interval, range within which the true population mean is expected to lie
 
     @classmethod
-    def from_ndarray(cls, data: np.ndarray) -> 'VariabilityMeasures':
+    def from_ndarray(cls,
+                     name: str,
+                     data: np.ndarray) -> 'VariabilityMeasures':
         if not data:
             raise ValueError("Sample list cannot be empty")
         if len(data.shape) != 1:
             raise ValueError("Sample list must be one-dimensional")
         return cls(
-            data=data,
             stddev=np.nanstd(data),
             mad=np.nanmedian(np.abs(data - np.nanmedian(data))),
             iqr=np.nanpercentile(data, 75) - np.nanpercentile(data, 25),
@@ -65,27 +99,34 @@ class VariabilityMeasures:
         )
 
     @classmethod
-    def from_samples(cls, samples: Samples) -> 'VariabilityMeasures':
+    def from_samples(cls,
+                     name: str,
+                     samples: SampleList) -> 'VariabilityMeasures':
         if not samples:
             raise ValueError("Sample list cannot be empty")
-        return cls.from_ndarray(data=np.array(samples))
+        return cls.from_ndarray(
+            name=name,
+            data=np.array(samples))
 
 
 @dataclass
-class SampleStatistics:
+class DescriptiveStatistics:
     """
     Descriptive statistics for a univariate measurement, such as a body segment length.
     "Univariate" means that there is only one variable being measured (i.e. the length of a body segment).
     """
-    data: np.ndarray
+    sample_data: SampleData
 
+    @classmethod
+    def from_samples(cls,
+                        name: str,
+                        samples: SampleList) -> 'DescriptiveStatistics':
+            return cls(
+                sample_data=SampleData.from_samples(name=name, samples=samples)
+            )
     @property
-    def samples(self) -> Samples:
+    def samples(self) -> SampleList:
         return self.data.tolist()
-
-    @property
-    def number_of_samples(self) -> int:
-        return len(self.samples)
 
     @property
     def measures_of_central_tendency(self) -> CentralTendencyMeasures:
@@ -98,6 +139,7 @@ class SampleStatistics:
     def __getattr__(self, name: str) -> float:
         """
         Delegate attribute access to the appropriate component class.
+        e.g, so that `self.mean` returns `self.measures_of_central_tendency.mean`
         """
         central_tendency = self.measures_of_central_tendency
         variability = self.measures_of_variability
@@ -107,12 +149,12 @@ class SampleStatistics:
         elif hasattr(variability, name):
             return getattr(variability, name)
         else:
-            raise AttributeError(f"'SampleStatistics' object has no attribute '{name}'")
+            raise AttributeError(f"{self.__class__.__name__} object has no attribute '{name}'")
 
 
 if __name__ == "__main__":
     samples = [1.0, 2.0, 3.0, 4.0, 5.0]
-    stats = SampleStatistics(samples)
+    stats = DescriptiveStatistics(sample_data=SampleData(name="test", data=np.array(samples)))
 
     print(stats.mean)  # Accessing mean from CentralTendencyMeasures
     print(stats.stddev)  # Accessing stddev from VariabilityMeasures
--- ajc27_freemocap_blender_addon/tests/test_statistics.py
+++ ajc27_freemocap_blender_addon/tests/test_statistics.py
@@ -1,50 +1,65 @@
-import pytest
 import numpy as np
-from typing import List
-from message import CentralTendencyMeasures, VariabilityMeasures, SampleStatistics
+import pytest
+
+from ajc27_freemocap_blender_addon.data_models.sample_statistics import SampleList, CentralTendencyMeasures, \
+    VariabilityMeasures, Des
 
 
 @pytest.fixture
-def sample_data() -> Samples:
+def sample_data_fixture() -> SampleList:
     return [1.0, 2.0, 3.0, 4.0, 5.0]
 
+
 @pytest.fixture
-def sample_ndarray(sample_data: Samples) -> np.ndarray:
-    return np.array(sample_data)
+def sample_ndarray_fixture(sample_data_fixture: SampleList) -> np.ndarray:
+    return np.array(sample_data_fixture)
 
-def test_central_tendency_measures_from_data(sample_ndarray: np.ndarray) -> None:
-    ctm = CentralTendencyMeasures.from_data(sample_ndarray)
-    assert ctm.mean == np.nanmean(sample_ndarray)
-    assert ctm.median == np.nanmedian(sample_ndarray)
 
-def test_central_tendency_measures_from_samples(sample_data: Samples) -> None:
+@pytest.fixture
+def sample_statistics_fixture(sample_data_fixture: SampleList) -> Des:
+    return Des.from_samples(sample_data_fixture)
+
+
+def test_central_tendency_measures_from_data(sample_ndarray_fixture: np.ndarray) -> None:
+    ctm = CentralTendencyMeasures.from_data(sample_ndarray_fixture)
+    assert ctm.mean == np.nanmean(sample_ndarray_fixture)
+    assert ctm.median == np.nanmedian(sample_ndarray_fixture)
+
+
+def test_central_tendency_measures_from_samples(sample_data: SampleList) -> None:
     ctm = CentralTendencyMeasures.from_samples(sample_data)
     assert ctm.mean == np.nanmean(sample_data)
     assert ctm.median == np.nanmedian(sample_data)
 
-def test_variability_measures_from_ndarray(sample_ndarray: np.ndarray) -> None:
-    vm = VariabilityMeasures.from_ndarray(sample_ndarray)
-    assert vm.stddev == np.nanstd(sample_ndarray)
-    assert vm.mad == np.nanmedian(np.abs(sample_ndarray - np.nanmedian(sample_ndarray)))
-    assert vm.iqr == np.nanpercentile(sample_ndarray, 75) - np.nanpercentile(sample_ndarray, 25)
-    assert vm.ci95 == 1.96 * np.nanstd(sample_ndarray) / np.sqrt(len(sample_ndarray))
 
-def test_variability_measures_from_samples(sample_data: Samples) -> None:
+def test_variability_measures_from_ndarray(sample_ndarray_fixture: np.ndarray) -> None:
+    vm = VariabilityMeasures.from_ndarray(sample_ndarray_fixture)
+    assert vm.stddev == np.nanstd(sample_ndarray_fixture)
+    assert vm.mad == np.nanmedian(np.abs(sample_ndarray_fixture - np.nanmedian(sample_ndarray_fixture)))
+    assert vm.iqr == np.nanpercentile(sample_ndarray_fixture, 75) - np.nanpercentile(sample_ndarray_fixture, 25)
+    assert vm.ci95 == 1.96 * np.nanstd(sample_ndarray_fixture) / np.sqrt(len(sample_ndarray_fixture))
+
+
+def test_variability_measures_from_samples(sample_data: SampleList) -> None:
     vm = VariabilityMeasures.from_samples(sample_data)
     assert vm.stddev == np.nanstd(sample_data)
     assert vm.mad == np.nanmedian(np.abs(sample_data - np.nanmedian(sample_data)))
     assert vm.iqr == np.nanpercentile(sample_data, 75) - np.nanpercentile(sample_data, 25)
     assert vm.ci95 == 1.96 * np.nanstd(sample_data) / np.sqrt(len(sample_data))
 
-def test_sample_statistics_properties(sample_data: Samples) -> None:
-    stats = SampleStatistics(np.array(sample_data))
+
+def test_sample_statistics_properties(sample_statistics_fixture: Des,
+                                      sample_data: SampleList) -> None:
+    stats = sample_statistics_fixture
     assert stats.samples == sample_data
     assert stats.number_of_samples == len(sample_data)
     assert isinstance(stats.measures_of_central_tendency, CentralTendencyMeasures)
     assert isinstance(stats.measures_of_variability, VariabilityMeasures)
 
-def test_sample_statistics_delegation(sample_data: Samples) -> None:
-    stats = SampleStatistics(np.array(sample_data))
+
+def test_sample_statistics_delegation(sample_statistics_fixture: Des,
+                                      sample_data: SampleList) -> None:
+    stats = sample_statistics_fixture
     assert stats.mean == np.nanmean(sample_data)
     assert stats.median == np.nanmedian(sample_data)
     assert stats.stddev == np.nanstd(sample_data)
@@ -52,22 +67,46 @@ def test_sample_statistics_delegation(sample_data: Samples) -> None:
     assert stats.iqr == np.nanpercentile(sample_data, 75) - np.nanpercentile(sample_data, 25)
     assert stats.ci95 == 1.96 * np.nanstd(sample_data) / np.sqrt(len(sample_data))
 
+
 def test_empty_data() -> None:
-    with pytest.raises(ValueError, match="Sample list cannot be empty"):
+    try:  # note - avoid `pytest.raises` and similar bc they are not thread safe 🙄
         CentralTendencyMeasures.from_data(np.array([]))
-    with pytest.raises(ValueError, match="Sample list cannot be empty"):
+    except ValueError as e:
+        assert str(e) == "Sample list cannot be empty"
+
+    try:
         CentralTendencyMeasures.from_samples([])
-    with pytest.raises(ValueError, match="Sample list cannot be empty"):
+    except ValueError as e:
+        assert str(e) == "Sample list cannot be empty"
+
+    try:
         VariabilityMeasures.from_ndarray(np.array([]))
-    with pytest.raises(ValueError, match="Sample list cannot be empty"):
+    except ValueError as e:
+        assert str(e) == "Sample list cannot be empty"
+
+    try:
         VariabilityMeasures.from_samples([])
+    except ValueError as e:
+        assert str(e) == "Sample list cannot be empty"
+
 
 def test_invalid_data_shape() -> None:
-    with pytest.raises(ValueError, match="Sample list must be one-dimensional"):
+    try:
         CentralTendencyMeasures.from_data(np.array([[1.0, 2.0], [3.0, 4.0]]))
-    with pytest.raises(ValueError, match="Sample list must be one-dimensional"):
+    except ValueError as e:
+        assert str(e) == "Sample list must be one-dimensional"
+
+    try:
         CentralTendencyMeasures.from_samples([[1.0, 2.0], [3.0, 4.0]])
-    with pytest.raises(ValueError, match="Sample list must be one-dimensional"):
+    except ValueError as e:
+        assert str(e) == "Sample list must be one-dimensional"
+
+    try:
         VariabilityMeasures.from_ndarray(np.array([[1.0, 2.0], [3.0, 4.0]]))
-    with pytest.raises(ValueError, match="Sample list must be one-dimensional"):
+    except ValueError as e:
+        assert str(e) == "Sample list must be one-dimensional"
+
+    try:
         VariabilityMeasures.from_samples([[1.0, 2.0], [3.0, 4.0]])
+    except ValueError as e:
+        assert str(e) == "Sample list must be one-dimensional"
