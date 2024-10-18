9c8abff4f95ff1627100b485860b4cc8be40cff1
ajc27 <andres.casals@gmail.com>
2024-10-17 22:57:02 -0300

    Fixed individual fbx materials and assigned fixed Base material

--- freemocap_blender_addon/core_functions/meshes/skelly_mesh/attach_skelly_mesh.py
+++ freemocap_blender_addon/core_functions/meshes/skelly_mesh/attach_skelly_mesh.py
@@ -161,10 +161,9 @@ def attach_skelly_by_bone_mesh(
         # Apply the transformations to the Skelly part
         bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
  
-    # Set material 'Base' as material 0 to all skelly meshes except the head
+    # Set material 'Base' as material 0 for all skelly bone meshes
     for skelly_mesh in skelly_meshes:
-        if skelly_mesh.name != 'Skelly_head':
-            skelly_mesh.data.materials[0] = bpy.data.materials['Base.009']
+        skelly_mesh.data.materials[0] = bpy.data.materials['Base']
 
     # Rename the first mesh to skelly_mesh
     skelly_meshes[0].name = "skelly_mesh"
