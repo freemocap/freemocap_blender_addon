9b83371902ce06b081249bf8515d93ceb1222d9f
Jon Matthis <jonmatthis@gmail.com>
2026-06-10 11:07:33 -0400

    add deps as arg

--- freemocap_blender_addon/utilities/install_dependencies.py
+++ freemocap_blender_addon/utilities/install_dependencies.py
@@ -4,10 +4,14 @@ import sys
 OPTIONAL_DEPENDENCIES = ["tomllib"] #, "opencv-contrib-python", "matplotlib"]
 
 
-def check_and_install_dependencies():
+def check_and_install_dependencies(dependencies:list[str]|None=None):
+    if dependencies is None:
+        dependencies = OPTIONAL_DEPENDENCIES
     print("Checking if required packages are installed...")
     # get path of blender internal python executable
     python_executable = str(sys.executable)
+
+
 
     try:
         try:
@@ -21,7 +25,7 @@ def check_and_install_dependencies():
         print("\nInstalled packages:", installed_packages, "\n")
 
         packages_to_install = []
-        for package_name in OPTIONAL_DEPENDENCIES:
+        for package_name in dependencies:
             if package_name in installed_packages:
                 print(f"{package_name} already installed!")
             else:
@@ -42,7 +46,7 @@ def check_and_install_dependencies():
         print("All required packages installed! Done!")
     except Exception as e:
         print(f"Error installing packages: {e}")
-        print(f"\n[FREEMOCAP-BLENDER-ADDON-INIT] - Optional dependencies {OPTIONAL_DEPENDENCIES} not installed, some functionality may not work! \n"
+        print(f"\n[FREEMOCAP-BLENDER-ADDON-INIT] - Optional dependencies {dependencies} not installed, some functionality may not work! \n"
               f"Check the error above for more information.\n"
               f" If it's a permissions issue, try running Blender as an administrator (Windows) or using sudo (Linux). "
               f"\nYou should only have to do this once, the packages will be installed and available for future Blender sessions.")
